0c9f59b3367cbb07a73c49cde880ea82ceb68df8
mzums <annsie035@gmail.com>
2025-06-23 21:07:36 +0200

    nicer ui, more responsve to screen size

--- src/ui/gui.rs
+++ src/ui/gui.rs
@@ -1,15 +1,14 @@
+use core::time;
 use std::collections::VecDeque;
-
 use macroquad::prelude::*;
+use std::time::Instant;
 
 use crate::utils;
 
 
-fn write_title(font: Option<Font>, font_size: f32) {
+fn write_title(font: Option<Font>, font_size: f32, x: f32, y: f32) {
 
     let (type_text, man_text) = ("Type", "Man");
-    let x = 50.0;
-    let y = 100.0;
 
     let type_width = measure_text(type_text, font.as_ref(), font_size as u16, 1.0).width;
 
@@ -71,7 +70,8 @@ pub async fn gui_main_async() {
     let top_words = 500;
     let word_list = utils::read_first_n_words(top_words as usize);
     let batch_size = 50;
-    let font_size = 40.0;
+    let mut timer = time::Duration::from_secs(0);
+    let start_time = Instant::now();
     
     let reference = utils::get_reference(
         false,
@@ -80,27 +80,26 @@ pub async fn gui_main_async() {
         batch_size,
     );
 
-    let max_width = screen_width() * 0.9;
-
-    let lines: Vec<String> = create_lines(&reference, font.clone(), font_size, max_width);
-
+    
     let mut pressed_vec: Vec<char>  = vec![];
     let mut is_correct: VecDeque<i8> = VecDeque::from(vec![0; reference.len()]); // 2 - correct, 1 - corrected, 0 - not typed, -1 - incorrect
     let mut pos: usize;
     let mut pos1: usize = 0;
     
     loop {
+        let font_size = 40.0;
+        let max_width = f32::min(screen_width() * 0.9, 1700.0);
+        let lines: Vec<String> = create_lines(&reference, font.clone(), font_size, max_width);
+
         clear_background(Color::from_rgba(20, 17, 15, 255));
         
         let total_height = lines.len() as f32 * font_size * 1.2;
         let start_y = screen_height() / 2.0 - total_height / 2.0 + font_size;
-        write_title(title_font.clone(), 50.0);
+        let start_x = screen_width() / 2.0 - max_width / 2.0;
+        write_title(title_font.clone(), f32::min(screen_width(), screen_height()) / 30.0, start_x, screen_height() / 15.0);
         
         let pressed = get_char_pressed();
         if pressed.is_some() {
-            //println!("{:?}", reference);
-            //println!("{:?} {:?} {:?}", pressed, reference.chars().nth(pos1 as usize), pos1);
-            println!("Pressed: {:?}", pressed);
             if pressed == Some('\u{8}') {
                 pressed_vec.pop();
                 if pos1 > 0 {
@@ -114,7 +113,6 @@ pub async fn gui_main_async() {
                 } else {
                     is_correct[pos1] = -1; // Incorrect
                 }
-                //println!("is_correct: {:?}", is_correct);
                 pos1 += 1;
                 pressed_vec.push(pressed.unwrap());
             }
@@ -123,7 +121,14 @@ pub async fn gui_main_async() {
         let mut type_height: f32;
         let mut type_width: f32;
         let mut pos_y = 0.0;
-        //println!("pos1: {}", pos1);
+
+        let timer_str = format!("{:.0}", 30.0 - timer.as_secs_f32());
+        draw_text_ex(&timer_str, start_x, start_y - screen_height() / 20.0, TextParams {
+            font: font.as_ref(),
+            font_size: font_size as u16,
+            color: Color::from_rgba(255, 155, 0, 255),
+            ..Default::default()
+        });
         
         for line in lines.iter() {
             let mut pos_x = 0;
@@ -134,6 +139,9 @@ pub async fn gui_main_async() {
                 } else if is_correct.get(pos).is_some() && is_correct[pos] == 2 {
                     Color::from_rgba(255, 255, 255, 200) // correct
                 } else if is_correct.get(pos).is_some() && is_correct[pos] == 1 {
+                    if char == ' ' {
+                        curr_char = '_';
+                    }
                     Color::from_rgba(255, 165, 0, 255) // corrected
                 } else {
                     if char == ' ' {
@@ -143,7 +151,7 @@ pub async fn gui_main_async() {
                 };
                 draw_text_ex(
                     &curr_char.to_string(),
-                    pos_x as f32 + 50.0,
+                    pos_x as f32 + start_x,
                     pos_y + start_y,
                     TextParams {
                         font: font.as_ref(),
@@ -164,6 +172,7 @@ pub async fn gui_main_async() {
             break;
         }
 
+        timer += start_time.elapsed() - timer;
         next_frame().await;
     }
 }
